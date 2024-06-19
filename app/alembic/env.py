eb7a3be391ccd14b21d47fef07f7e037b6c24920
viniciussilva4 <vinicius.silva.ribeiro.suiciniv@gmail.com>
2024-06-19 17:05:41 -0300

    atualização github

--- app/alembic/env.py
+++ app/alembic/env.py
@@ -17,8 +17,9 @@ DB_HOST = os.getenv('DB_HOST')
 DB_USER = os.getenv('DB_USER')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
 DB_NAME = os.getenv('DB_NAME')
+DB_PORT = os.getenv('DB_PORT')
 
-DATABASE_URL = f'postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:5432/{DB_NAME}'
+DATABASE_URL = f'postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
 
 config = context.config
 fileConfig(config.config_file_name)
--- app/persistence/db_utils.py
+++ app/persistence/db_utils.py
@@ -8,10 +8,11 @@ DB_HOST = os.getenv('DB_HOST')
 DB_USER = os.getenv('DB_USER')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
 DB_NAME = os.getenv('DB_NAME')
+DB_PORT = os.getenv('DB_PORT')
 
 def get_engine():
 
-  db_url = f'postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:5432/{DB_NAME}'
+  db_url = f'postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
   engine = create_engine(db_url, echo = True)
   
   return engine
